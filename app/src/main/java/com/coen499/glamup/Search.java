61ba7c7def5d9f4acc1ebd6213a3864377876f65
varna-vincent <vincent.varna@gmail.com>
2019-06-07 12:04:32 -0700

    Search UI

--- app/src/main/java/com/coen499/glamup/Search.java
+++ app/src/main/java/com/coen499/glamup/Search.java
@@ -5,7 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.Serializable;
 
 public class Search extends AppCompatActivity {
 
@@ -38,5 +43,33 @@ public class Search extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.getMenu().findItem(R.id.navigation_search).setChecked(true);
+
+        Toolbar mTopToolbar = (Toolbar) findViewById(R.id.search_toolbar);
+        setSupportActionBar(mTopToolbar);
+    }
+
+    public void search(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.review, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
